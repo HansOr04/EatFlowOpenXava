a8900121fa68a1f52817eb5b024321199fa4d4de
HansOr04 <hansalazar04@gmail.com>
2026-06-01 21:37:57 -0500

    feat: :sparkles: Add AdminRole and OrderStatus enums for role and order management

---
+++ src/main/java/com/grupo4/Eatflow/modelo/enums/AdminRole.java
@@ -0,0 +1,5 @@
+package com.grupo4.Eatflow.modelo.enums;
+
+public enum AdminRole {
+    SUPER_ADMIN, MANAGER, STAFF
+}
---
+++ src/main/java/com/grupo4/Eatflow/modelo/enums/OrderStatus.java
@@ -0,0 +1,5 @@
+package com.grupo4.Eatflow.modelo.enums;
+
+public enum OrderStatus {
+    PENDING, IN_PROGRESS, SERVED, CANCELLED
+}
